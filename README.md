240da1657842990542bf0afdd9ff0e8610abe9b6
Jorik2018 <antoniopinedo2018@gmail.com>
2024-05-12 18:27:57 -0500

    x

---
+++ README.md
@@ -0,0 +1,3 @@
+https://medium.com/@raditzlawliet/build-crud-rest-api-with-rust-and-mysql-using-axum-sqlx-d7e50b3cd130
+
+cargo watch -q -c -w src/ -x run
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+mod handler;
+mod model;
+mod route;
+mod schema;
+
+use std::sync::Arc;
+
+use axum::http::{header::CONTENT_TYPE, Method};
+
+use dotenv::dotenv;
+use tokio::net::TcpListener;
+
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+use route::create_router;
+use tower_http::cors::{Any, CorsLayer};
+
+pub struct AppState {
+    db: MySqlPool,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    println!("🌟 REST API Service 🌟");
+    let default_port = "8080";
+    let port = std::env::var("PORT").unwrap_or_else(|_| default_port.to_string());
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must set");
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅ Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("❌ Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any)
+        .allow_headers([CONTENT_TYPE]);
+
+    let app = create_router(Arc::new(AppState { db: pool.clone() })).layer(cors);
+
+    println!("✅ Server started successfully at 0.0.0.0:{}", port);
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
